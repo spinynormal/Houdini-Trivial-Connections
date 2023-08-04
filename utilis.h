4042d5f03882e858ddec88140081aa64bcfab44b
spinynormal <33784562+spinynormal@users.noreply.github.com>
2023-08-04 14:10:01 +0200

    Create utilis.h

---
+++ utilis.h
@@ -0,0 +1,62 @@
+int edgenmr (int hedge){
+    int vtxprim = vertexprim(0 , hedge); 
+    int vtxpt = vertexprimindex(0 , hedge); 
+    int edge = vertex( 0 , "edge" , vtxprim,vtxpt); 
+    return edge; 
+}
+
+int hedgefromedge(int hedge){
+    int vtxprim = vertexprim(0 , hedge); 
+    int vtxpt = vertexprimindex(0 , hedge); 
+    int src = vertex( 0 , "src" , vtxprim,vtxpt); 
+    int dst = vertex( 0 , "dst" , vtxprim,vtxpt); 
+    int newhedge = hedge_srcvertex(0,hedge); 
+    return newhedge;
+} 
+
+int signs(int hedge){
+    int vtxprim = vertexprim(0 , hedge); 
+    int vtxpt = vertexprimindex(0 , hedge); 
+    int sign = vertex( 0 , "sign" , vtxprim,vtxpt); 
+    return sign;
+} 
+
+
+vector vectorhedge(int h ){
+
+    int srca = hedge_srcpoint(0 , h); 
+    int dsta = hedge_dstpoint(0, h);
+    
+    vector va = point( 0 , "P", srca); 
+    vector vb = point( 0 , "P", dsta);  
+    vector hedgesg = normalize(vb-va); 
+    return hedgesg; 
+    
+}   
+
+void visualize_vector(vector viz; int prim; string name) {
+
+
+int primspts[]  = primpoints(0, prim) ; 
+vector a = point( 0 , "P", primspts[0]);
+vector b = point( 0 , "P", primspts[1]);
+vector c = point( 0 , "P", primspts[2]);
+
+vector center = (a+b+c) / 3.0; 
+
+int pt = addpoint(0,center); 
+
+setpointattrib(0, name, pt, viz);
+
+}
+
+void gram_schmidt(int prim; vector ei, ej){
+
+int hedgeprim = primhedge(0, prim);
+ei = vectorhedge(hedgeprim);
+
+vector normal = prim( 0 , "N", prim); 
+
+ej = cross(normal,ei ); 
+
+}
